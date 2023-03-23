e919210715a37ff1e79fe64ad42633e37b11474b
Tendeeno <95103105+Tendeeno@users.noreply.github.com>
2023-03-23 12:38:05 -0400

    add arb price

--- components/table.js
+++ components/table.js
@@ -14,24 +14,6 @@
 const Table = ({ daos, arbPrice }) => {
   return (
     <div className="px-4 sm:px-6 lg:px-8 container mx-auto">
-      {/* <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Users
-          </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 py-2 px-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Add user
-          </button>
-        </div>
-      </div> */}
       <div className="mt-8 flow-root">
         <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import axios from "axios";
 
 export default function Home() {
   const getArbPrice = async () => {
+    console.log("getting price");
     const { data } = await axios.get(
       "https://coins.llama.fi/prices/current/arbitrum:0x912ce59144191c1204e64559fe8253a0e49e6548?searchWidth=4h"
     );
@@ -18,11 +19,29 @@ export default function Home() {
 
   useEffect(() => {
     getArbPrice();
+    const interval = setInterval(() => getArbPrice(), 7000);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
+
+  // useEffect(() => {
+  //   getArbPrice();
+  // }, []);
 
   const [arbPrice, setArbPrice] = useState(1.1);
   return (
     <div className="bg-gray-100 py-12">
+      <div className="px-4 sm:px-6 lg:px-8 container mx-auto">
+        <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+          <dt className="truncate text-sm font-medium text-gray-500">
+            Current ARB price
+          </dt>
+          <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+            {arbPrice}
+          </dd>
+        </div>
+      </div>
       <Table daos={airdropTotals} arbPrice={arbPrice} />
       {airdropTotals.map(({ id, daoName, airdropAmount }) => {
         return (
